c3313de0811014a7200480d3a8328790c5f38ef6
campagnarinicholas <campagnarinicholas@gmail.com>
2022-09-02 08:36:53 -0700

    init

--- src/main/java/hu/modeldriven/cameo/Lesson1Plugin.java
+++ src/main/java/hu/modeldriven/cameo/Lesson1Plugin.java
@@ -1,5 +1,6 @@
 package hu.modeldriven.cameo;
 
+import com.nomagic.magicdraw.actions.ActionsConfiguratorsManager;
 import com.nomagic.magicdraw.core.Application;
 import com.nomagic.magicdraw.plugins.Plugin;
 
@@ -7,9 +8,15 @@ public class Lesson1Plugin extends Plugin {
 
     @Override
     public void init() {
+        createMainMenuAction();
         Application.getInstance().getGUILog().showMessage("Hello world from Lesson1");
     }
 
+    private void createMainMenuAction() {
+        var action = new MainMenuAction("Lesson2MainMenuAction", "Lesson 2 Main Menu action");
+        var configurator = new MainMenuConfiguration(action);
+        ActionsConfiguratorsManager.getInstance().addMainMenuConfigurator(configurator);
+    }
     @Override
     public boolean close() {
         return true;
